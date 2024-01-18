8a736c11309f073f9194cf69052091b2cb042237
Moses613 <44594121+Moses613@users.noreply.github.com>
2024-01-18 17:12:10 -0500

    Update Ubuntu script

--- Projects/linux-initial-setup.sh
+++ Projects/linux-initial-setup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 #Ubuntu
+#Install stuff
+sudo apt update
+sudo apt-get install cowsay cowsay-off fortune git gh tmux sl lolcat cava
+
+#Configure tmux
+cd ~
+wget https://raw.githubusercontent.com/Moses613/random-code-projects/main/Configs/.tmux.conf
+
+#configure git
+git config --global core.editor "nano"
+git config --global user.name "Moses613"
+git config --global user.email "44594121+Moses613@users.noreply.github.com" 
+
+#Messages 
+echo "Remember to authorize your GitHub account"
+
