1230abb6e216a570b47ab384d425e6ad63eea3f2
Moses613 <44594121+Moses613@users.noreply.github.com>
2024-01-18 18:42:03 -0500

    add breakpoint messages

--- Projects/linux-initial-setup.bash
+++ Projects/linux-initial-setup.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Make sure to run this in bash, not accidentally override with sh"
+echo
 
 #Check if shell has root
 current_user=$(whoami)
@@ -15,15 +16,22 @@ fi
 sudo apt update
 sudo apt upgrade
 sudo apt-get install cowsay cowsay-off fortune git gh tmux sl lolcat cava
+echo
+echo
 
 #Configure tmux
 cd ~
 wget https://raw.githubusercontent.com/Moses613/random-code-projects/main/Configs/.tmux.conf
+echo
+echo "tmux config downloaded to home directory"
+echo
 
 #configure git
 git config --global core.editor "nano"
 git config --global user.name "Moses613"
 git config --global user.email "44594121+Moses613@users.noreply.github.com" 
+echo "git configured"
+echo
 
 #Messages 
 echo "Remember to authorize your GitHub account"
